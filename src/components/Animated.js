20e66104f13b64dabecd91d7089c22fb9a9986af
biantaiwen <biantaiwen@163.com>
2017-06-06 22:20:45 +0800

    修改Animated 06062220

--- src/components/Animated.js
+++ src/components/Animated.js
@@ -14,11 +14,43 @@ class AnimatedComponent extends Component {
     constructor (props) {
         super (props)
         this.state={
-            animOpacity:new Animated.Value(0)
+            animOpacity:new Animated.Value(0),
+            animImageScale:new Animated.Value(0),
+            animTranslateX:new Animated.Value(0),
+            animWidth1:new Animated.Value(100),
+            animWidth2:new Animated.Value(100),
+            animWidth3:new Animated.Value(100),
         }
-        this.textOpacity1= this.textOpacity1.bind(this)
     }
     componentDidMount(){
+
+    }
+    parallelAnim (){
+        this.state.animWidth1.setValue(100)
+        this.state.animWidth2.setValue(100)
+        this.state.animWidth3.setValue(100)
+        let parallel = Animated.parallel([
+            Animated.timing(
+                this.state.animWidth1,
+                {
+                    toValue:30,
+                    duration: 4000,
+                    easing: Easing.linear
+                }),
+            Animated.spring(
+                this.state.animWidth2,
+                {
+                    toValue:300,
+                    friction:2,
+                    tension:40
+                }),
+            Animated.timing(
+                this.state.animWidth3,
+                {
+                    toValue:300,
+                }),
+        ],{stopTogether:true});
+        parallel.start((a)=>{console.log('动画执行完毕',a)})
 
     }
     textOpacity1 (){
@@ -33,9 +65,45 @@ class AnimatedComponent extends Component {
         )
         timing.start()
     }
+    imageSpring () {
+        let spring = Animated.spring(
+            this.state.animImageScale,
+            {
+                toValue:1,
+                friction:7,
+                tension:40
+            }
+        )
+        spring.start()
+    }
+    viewTranslateX (){
+        // let timing = Animated.timing(
+        //     this.state.animTranslateX,
+        //     {
+        //         toValue:150,
+        //         duration: 4000,
+        //         easing: Easing.linear
+        //     }
+        // )
+        // timing.start()
+        this.state.animTranslateX.setValue(150)
+        let decay = Animated.decay(
+            this.state.animTranslateX,
+            {
+                toValue:150,
+                velocity:10,
+                deceleration: 0.6,
+
+            }
+        )
+        decay.start()
+    }
+
     render () {
         return (
-        <ScrollView>
+        <ScrollView
+            contentContainerStyle={styles.container}
+        >
 
             <Animated.View
                 style={{
@@ -44,14 +112,49 @@ class AnimatedComponent extends Component {
             >
                 <Text style={{fontSize:40}}>textOpacity1</Text>
             </Animated.View>
-            <TouchableOpacity onPress={this.textOpacity1}>
+            <TouchableOpacity onPress={this.textOpacity1.bind(this)}>
                 <Text style={{fontSize:30,color:'#333'}}>淡入</Text>
             </TouchableOpacity>
 
             <Animated.Image
-                style={{ width: 227, height: 200,transform:[{perspective:200}]}}
+                style={{
+                    width: 227,
+                    height: 200,
+                    transform:[
+                        {
+                            scale:this.state.animImageScale.interpolate({
+                                inputRange:[0,0.5,1],
+                                outputRange:[1,0.5,1]
+                            })
+                        }
+                    ]
+                }}
                 source={{uri: 'http://pic.58pic.com/58pic/10/96/99/18u58PICXCS.jpg'}}/>
-
+            <TouchableOpacity onPress={this.imageSpring.bind(this)}>
+                <Text style={{fontSize:30,color:'#333'}}>弹跳</Text>
+            </TouchableOpacity>
+            <Animated.View style={{width:30,height:30,backgroundColor:'#ccc',transform:[{translateX:this.state.animTranslateX}]}}>
+            </Animated.View>
+            <TouchableOpacity onPress={this.viewTranslateX.bind(this)}>
+                <Text style={{fontSize:30,color:'#333'}}>移动</Text>
+            </TouchableOpacity>
+            <View style={{alignSelf:'flex-start'}}>
+                <Animated.View
+                    style={{width:this.state.animWidth1,height:30,backgroundColor:'#ccc',marginVertical:10}}
+                >
+                </Animated.View>
+                <Animated.View
+                    style={{width:this.state.animWidth2,height:30,backgroundColor:'#ccc',marginVertical:10}}
+                >
+                </Animated.View>
+                <Animated.View
+                    style={{width:this.state.animWidth3,height:30,backgroundColor:'#ccc',marginVertical:10}}
+                >
+                </Animated.View>
+            </View>
+            <TouchableOpacity onPress={this.parallelAnim.bind(this)}>
+                <Text style={{fontSize:30,color:'#333'}}>同时执行</Text>
+            </TouchableOpacity>
         </ScrollView>
 
         )
@@ -63,4 +166,8 @@ class AnimatedComponent extends Component {
 export default AnimatedComponent
 
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        alignItems:'center'
+    }
+})
